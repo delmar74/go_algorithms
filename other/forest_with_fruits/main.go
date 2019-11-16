b5f0393bb95cb37596bf86786e1acb54e9f848ee
Oleg Sh <os.delmar@gmail.com>
2019-11-16 20:05:05 +0300

    Run go fmt..

--- other/forest_with_fruits/main.go
+++ other/forest_with_fruits/main.go
@@ -1,43 +1,42 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
 )
 
 func generateLongestSequence(tree []int) (int, int) {
-  pre := 0
-  count := 1 
-  max := 1  // local maximum
-  seq := 1 // [result] max sequence 
-  pos := 0 // [result] finding position
+	pre := 0
+	count := 1
+	max := 1 // local maximum
+	seq := 1 // [result] max sequence
+	pos := 0 // [result] finding position
 
-  for j := 0; j<= len(tree)-2; j++ {
-    if tree[j] == tree[j+1] {
-      count++
-    } else {
-      max = max + count
+	for j := 0; j <= len(tree)-2; j++ {
+		if tree[j] == tree[j+1] {
+			count++
+		} else {
+			max = max + count
 
-      if seq < max {
-        seq = max
-        pos = j-max+1
-      }
+			if seq < max {
+				seq = max
+				pos = j - max + 1
+			}
 
-      if pre != tree[j+1] {
-        max = count
-      }
+			if pre != tree[j+1] {
+				max = count
+			}
 
-      count=1
-      pre = tree[j]
-    }
-  }
-  return seq, pos
+			count = 1
+			pre = tree[j]
+		}
+	}
+	return seq, pos
 }
 
 func main() {
-  tree := []int{2, 4, 4, 2, 2, 4, 2, 4, 4, 2, 3, 2, 1, 1, 1, 2, 1, 2, 3, 4, 3, 2, 1}
-  seq, pos := generateLongestSequence(tree)
+	tree := []int{2, 4, 4, 2, 2, 4, 2, 4, 4, 2, 3, 2, 1, 1, 1, 2, 1, 2, 3, 4, 3, 2, 1}
+	seq, pos := generateLongestSequence(tree)
 
-  fmt.Println(tree)
-  fmt.Println("seq = ", seq, ", pos = ", pos)
+	fmt.Println(tree)
+	fmt.Println("seq = ", seq, ", pos = ", pos)
 }
-
--- other/forest_with_fruits/main_test.go
+++ other/forest_with_fruits/main_test.go
@@ -5,9 +5,8 @@ import "testing"
 func TestName(t *testing.T) {
 
 	tree := []int{2, 4, 4, 4, 2, 3, 2, 1, 1, 1, 2, 1, 2, 3, 4, 3, 2, 1}
-  seq := 7
-  pos := 6
-
+	seq := 7
+	pos := 6
 
 	res_seq, res_pos := generateLongestSequence(tree)
 	if res_seq != seq || res_pos != pos {
